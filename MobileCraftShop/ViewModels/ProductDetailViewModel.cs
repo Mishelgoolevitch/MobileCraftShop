41f628b7a1fffcdb313b63360b104b424096e9c3
mixail <v1tus5555@yandex.by>
2026-06-06 10:30:17 +0300

    Создаем класс ProductDetailViewModel

---
+++ MobileCraftShop/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MobileCraftShop.Models;
+
+namespace MobileCraftShop.ViewModels
+{
+    public class ProductDetailViewModel
+    {
+        // Основной просматриваемый продукт
+        public Product Product { get; set; } = null!;
+
+        // Список похожих товаров (например, из той же торговой марки или категории)
+        public List<Product> RelatedProducts { get; set; } = new List<Product>();
+
+        // Используется для переключения значка сердечка, если пользователь уже сохранил этот элемент
+        public bool IsInWishlist { get; set; }
+    }
+}
