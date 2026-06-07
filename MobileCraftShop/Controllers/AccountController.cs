52489aeb2f2d38da398713f2e4c874ee37c91878
mixail <v1tus5555@yandex.by>
2026-06-07 08:32:52 +0300

    Создаем класс RegisterViewModel и контроллер AccountController

---
+++ MobileCraftShop/Controllers/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MobileCraftShop.Data;
+using MobileCraftShop.Models;
+using MobileCraftShop.Services;
+using Stripe;
+
+namespace MobileCraftShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ApplicationDbContext _context;  // ADD THIS
+        private readonly IShoppingCartService _cartService;
+
+        public AccountController(
+           UserManager<ApplicationUser> userManager,
+           SignInManager<ApplicationUser> signInManager,
+           RoleManager<IdentityRole> roleManager,
+           ApplicationDbContext context,  // ADD THIS PARAMETER)
+           IShoppingCartService cartService)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _context = context;  // ADD THIS
+            _cartService = cartService;
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register(string? returnUrl = null)
+        {
+            // 1. Preserving the intended navigation path
+            ViewData["ReturnUrl"] = returnUrl;
+
+            // 2. Rendering the onboarding interface
+            return View();
+        }
+    }
+}
