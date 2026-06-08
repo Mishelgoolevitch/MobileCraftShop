3f0d693cd898079b1a1d851c074b02eb405a5658
mixail <v1tus5555@yandex.by>
2026-06-08 11:18:23 +0300

    Добавляем класс CheckoutViewModel.cs и контроллер CheckoutController.cs и добовляем представление Index.cshtml

---
+++ MobileCraftShop/Controllers/CheckoutController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MobileCraftShop.Models;
+using MobileCraftShop.Services;
+using MobileCraftShop.ViewModels;
+
+namespace MobileCraftShop.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly IShoppingCartService _cartService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CheckoutController(
+            IShoppingCartService cartService,
+            UserManager<ApplicationUser> userManager)
+        {
+            _cartService = cartService;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var cart = await _cartService.GetCartAsync();
+            if (cart.CartItems.Count == 0)
+            {
+                TempData["Error"] = "Your cart is empty.";
+                return RedirectToAction("Index", "ShoppingCart");
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            var model = new CheckoutViewModel
+            {
+                Cart = cart,
+                IsAuthenticated = user != null,
+                FirstName = user?.FirstName ?? "",
+                LastName = user?.LastName ?? "",
+                Email = user?.Email ?? "",
+                Phone = user?.PhoneNumber ?? "",
+                Address = user?.Address ?? "",
+                City = user?.City ?? "",
+                PostalCode = user?.PostalCode ?? "",
+                Country = user?.Country ?? "Pakistan"
+            };
+
+            ViewBag.CartItemCount = cart.ItemCount;
+            return View(model);
+        }
+    }
+}
---
+++ MobileCraftShop/Controllers/CheckoutViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MobileCraftShop.Controllers
+{
+    public class CheckoutViewModel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
